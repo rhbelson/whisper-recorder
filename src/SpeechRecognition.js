7d1b6a682656fec49530de0662bed2c746ccb5c2
Robbie Belson <rhbelson@gmail.com>
2019-04-29 21:28:19 -0500

    first commit

---
+++ src/SpeechRecognition.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import SpeechRecognition from "react-speech-recognition";
+import { AwesomeButton } from "react-awesome-button";
+// import AwesomeButtonProgress from 'react-awesome-button/src/components/AwesomeButtonProgress';
+import AwesomeButtonStyles from "react-awesome-button/src/styles/styles.scss";
+
+
+
+const propTypes = {
+  // Props injected by SpeechRecognition
+  transcript: PropTypes.string,
+  resetTranscript: PropTypes.func,
+  browserSupportsSpeechRecognition: PropTypes.bool
+};
+
+const Dictaphone = ({
+  transcript,
+  resetTranscript,
+  browserSupportsSpeechRecognition
+}) => {
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
+
+  function doSomethingThenCall() {
+    console.log("Calling endpoint");
+    fetch("http://hinckley.cs.northwestern.edu/~rbi054/whisper/update_answer.php",
+      {
+  method: 'POST',
+  headers: new Headers({
+             'Content-Type': 'application/x-www-form-urlencoded', // <-- Specifying the Content-Type
+    }),
+  body: `param1=${transcript}` // <-- Post parameters
+})
+    .then(function (responseObject) {
+                    return responseObject
+                }).then(function (result){
+                    console.log(result.text());
+                }).catch(function (error) {
+                    console.log('Request failed', error);
+                });
+  }
+
+  return (
+    <div>
+      
+      <p>Your Question: {transcript}</p> 
+          <AwesomeButton
+            size="large"
+            type="primary"
+            action={(element, next) => doSomethingThenCall()}>
+            Ask Question!
+          </AwesomeButton> 
+              <AwesomeButton
+            size="large"
+            type="secondary">
+            <span onClick={resetTranscript}>Clear Question</span>
+          </AwesomeButton> 
+    </div>
+  );
+};
+
+Dictaphone.propTypes = propTypes;
+
+export default SpeechRecognition(Dictaphone);
